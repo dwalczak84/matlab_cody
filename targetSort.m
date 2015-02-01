16f1478ab64afed2e1dace1eac71588d37d25968
Dariusz Walczak <dariusz.w84@gmail.com>
2015-02-01 20:44:32 +0000

    Create targetSort.m

---
+++ targetSort.m
@@ -0,0 +1,13 @@
+% Problem 94. Target sorting
+% Sort the given list of numbers a according to how far away each element 
+% is from the target value t. The result should return the list sorted in 
+% descending order of the absolute value of the difference between a(n) and t.
+% 
+% So if a = [1 2 4 8 17] and t = 12, then the output b should be [1 2 4 17 8].
+
+function ans = targetSort(a,t)
+
+sortrows([a; abs(a-t)]',-2);
+ans(:,1)';
+
+end
