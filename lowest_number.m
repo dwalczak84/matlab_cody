daa5743b3ec9e64cb44a871917bd847afd3eb2f6
Dariusz Walczak <dariusz.w84@gmail.com>
2015-03-20 21:15:34 +0000

    Update lowest_number.m

--- lowest_number.m
+++ lowest_number.m
@@ -7,11 +7,10 @@
 % smallest possible sum of two numbers that are the same length.
 % 
 % For example, if we're given [0 1 2 3], the smallest sum we can get using 
-%     those numbers by adding two 2-digit numbers together is 33. That's 
-%     either 10+23 or 13+20. We don't need to know what the two original 
-%     numbers are, just what their sum is. We also can't start any of our 
-%     numbers with a zero, because that's cheating.
-% 
+% those numbers by adding two 2-digit numbers together is 33. That's either
+% 10+23 or 13+20. We don't need to know what the two original numbers are, 
+% just what their sum is. We also can't start any of our numbers with a 
+% zero, because that's cheating.
 % You guys did a great job with my earlier problems, and even taught my 
 % dad a few things about MATLAB! I hope you can help me out with this one 
 % as well!
